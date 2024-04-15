0b4002178b3e0643ef9d91bc46d552fccbc04eea
shekhargrace14 <shekhargrace14@gmail.com>
2024-04-15 07:43:51 +0530

    just code

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const userCardTemplate = document.querySelector("[data-user-template]")
+const userCardContainer = document.querySelector("[data-user-cards-container]")
+const searchInput = document.querySelector("[data-search ]")
+
 
 fetch("https://jsonplaceholder.typicode.com/users")
 .then(resolve=>resolve.json())
 .then(data=>{
     console.log(data)
-    data.array.forEach(element => {
-        
+    data.forEach(element => { 
+        const card = userCardTemplate.content.cloneNode(true).children[0];
+        console.log(card)
+        const header = card.querySelector('header');
+        const body = card.querySelector('body');
+        header.textContent
+
+
     });
-    const card = userCardTemplate.content.cloneNode(true).children[0];
-    console.log(card)
 })
